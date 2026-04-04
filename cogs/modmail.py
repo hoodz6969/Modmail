48abd62d1b299cf41ab53bd1291ce5cfb0941a35
hoodz6969 <kyerobinson88@icloud.com>
2026-04-04 20:14:48 +0100

    Update modmail.py
    Signed-off-by: hoodz6969 <kyerobinson88@icloud.com>

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -2987,7 +2987,7 @@ class Modmail(commands.Cog):
             "Choose a designer to make your gun.\n"
             "Available Designers <@587650157630193669> <@1296955108755509271>\n"
             "\n"
-            "1. Pick a gun model from https://discord.com/channels/1165698510272856094/1473078405413998634\n"
+            "1. Pick a gun model from https://discord.com/channels/1165698510272856094/1487568930573914178\n"
             "Pistol £25\n"
             "SMG £30\n"
             "AR £35\n"
