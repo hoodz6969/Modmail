f860ca5a9f5048375f00373ea6151c3d9c404c15
hoodz6969 <kyerobinson88@icloud.com>
2026-04-26 11:30:23 +0100

    Add envy command to modmail for payment info
    Signed-off-by: hoodz6969 <kyerobinson88@icloud.com>

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -2888,6 +2888,27 @@ class Modmail(commands.Cog):
     @commands.command()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
+    async def envy(self, ctx):
+        closing_message = (
+            "🕶️ Envy's Design's\n"
+            "Payments accepted via PayPal.\n"
+            "PAYPALME LINK\n"
+            "\n"
+            "Please ensure to use the family and friends option\n"
+            "\n"
+            "THE COLLECTIVE • Secure & Trusted"
+        )
+
+        ctx.message.content = closing_message
+        async with ctx.typing():
+            await ctx.thread.reply(ctx.message)
+
+        sent_emoji, _ = await self.bot.retrieve_emoji()
+        await self.bot.add_reaction(ctx.message, sent_emoji)
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @checks.thread_only()
     async def chain(self, ctx):
         closing_message = (
             "Getting Started,\n"
